9841e5a60747c7c2c15b6321a3a3563d16a15a24
etylu <etlu03@gmail.com>
2022-08-25 20:19:56 -0400

    8/25 progress

--- linter.py
+++ linter.py
@@ -50,12 +50,14 @@ class Linter:
                     msg='Parts of this linter may not work without Python 3',
                     txt='Consider upgrading to Python 3.'
       )
+
       self.internal_print(exception, file_name)
     if(minor < 9):
       exception = Exception(
                     msg='Parts of this linter may not work without a modern Python',
                     txt='Consider upgrading to Python 3.9.'
       )
+
       self.internal_print(exception, file_name)
 
   def lint_lines(self, file_name):
@@ -69,7 +71,9 @@ class Linter:
                         msg='Line width exceeds 80 characters',
                         expl='You may not have a line of code longer than 80 characters.'
           )
+
           self.internal_print(exception, file_name)
+
         no += 1
 
   def run(self, source_path):
@@ -96,10 +100,9 @@ class Sets(Rule):
 
       val = elem.value
       if val in seen_values:
-        values = ', '.join([str(elem.value) for elem in node.elts if isinstance(elem, ast.Constant)])
         exception = Exception(
                       line=elem.lineno,
-                      txt='{' + f'{values}' + '}',
+                      txt='{name!r}',
                       msg='Set contains duplicate elements',
                       expl='Set may not explicitly be defined with duplicate elements'
         )
@@ -108,27 +111,78 @@ class Sets(Rule):
       else:
         seen_values.add(val)
 
+class Imports(Rule):
+  def __init__(self):
+    self.index = -1;
+
+    super().__init__()
+
+  def visit_Import(self, node):
+    packages = node.names
+
+    if self.index < node.lineno:
+      if 3 < (node.lineno - self.index):
+        exception = Exception(
+                      line=node.lineno,
+                      txt=None,
+                      msg='Import statements are spread out too much',
+                      expl='Declare imports at the top of every file'
+        )
+
+        self.exceptions.add(exception)
+
+    self.index = node.lineno
+
+    if 1 < len(packages):
+      error = ', '.join([package.name for package in packages])
+      exception = Exception(
+                      line=node.lineno,
+                      txt=f'import {error}',
+                      msg='Import more than one module on a single line',
+                      expl='Have only one import per line'
+      )
+
+      self.exceptions.add(exception)
+
 class Naming(Rule):
   def __init__(self):
     self.VAR_STYLE = '[A-Z]|[@!#$%^&*()<>?/\|}{~:]'
     self.FUNC_STYLE = '[A-Z]|[@!#$%^&*()<>?/\|}{~:]'
-    self.CLS_STYLE = '[0-9]|[@_!#$%^&*()<>?/\|}{~:]'
-    self.MOD_STYLE = '[A-Z]|[0-9]|[@_!#$%^&*()<>?/\|}{~:]'
-    self.PACK_STYLE = '[A-Z]|[0-9]|[@_!#$%^&*()<>?/\|}{~:]'
+    self.CLASS_STYLE = '[0-9]|[@_!#$%^&*()<>?/\|}{~:]'
+    self.PACKAGE_STYLE = '[A-Z]|[0-9]|[@_!#$%^&*()<>?/\|}{~:]'
+
+    self.default_expression = '{variable}={value}'
+
     super().__init__()
 
-  def lint_args(self, func, node):
+  def lint_args(self, node):
     arguments = node.args.args
     for arg in arguments:
       argument = arg.arg
       if re.search(self.VAR_STYLE, argument):
-        args = ', '.join([arg.arg for arg in arguments])
+        defaults = node.args.defaults
+        offset = len(arguments) - len(defaults)
+
+        text = list()
+        for index in range(len(arguments)):
+          if index < offset:
+            text.append(arguments[index].arg)
+          else:
+            text.append(self.default_expression.format(
+                                      variable=arguments[index].arg, 
+                                      value=str(defaults[index-offset].value)
+                        )
+            )
+
+        statement = ', '.join(text)
+
         exception = Exception(
                       line=node.lineno,
-                      txt='{name!r}',
+                      txt=f'{node.name}({statement})',
                       msg='Function arguments do not follow PEP8 Standards',
                       expl='Review PEP8 style for argument definition'
         )
+
         self.exceptions.add(exception)
         break
 
@@ -142,12 +196,14 @@ class Naming(Rule):
                       msg='Variable does not follow PEP8 Standards',
                       expl='Review PEP8 style for for variable definition'
         )
+
         self.exceptions.add(exception)
+
     super().generic_visit(node)
   
   def visit_FunctionDef(self, node):
     name = node.name
-    self.lint_args(name, node)
+    self.lint_args(node)
     if len(name) < 2 or re.search(self.FUNC_STYLE, name):
       exception = Exception(
                     line=node.lineno,
@@ -155,37 +211,47 @@ class Naming(Rule):
                     msg='Function name does not follow PEP8 Standards',
                     expl='Review PEP8 style for function definition'
       )
+
       self.exceptions.add(exception)
+
     super().generic_visit(node)
   
   def visit_ClassDef(self, node):
     name = node.name
-    if len(name) < 2 or re.search(self.CLS_STYLE, name):
+    if len(name) < 2 or re.search(self.CLASS_STYLE, name):
       exception = Exception(
                     line=node.lineno,
-                    txt=f'class {name}',
+                    txt=f'class {name}()',
                     msg='Class name does not follow PEP8 Standards',
                     expl='Review PEP8 style for class definition'
       )
+
       self.exceptions.add(exception)
+
     super().generic_visit(node)
   
   def visit_Import(self, node):
-    name = node.name
-    if len(name) < 2 or re.search(self.MOD_STYLE, name):
-      exception = Exception(
+    packages = node.names
+    if len(packages) == 1:
+      alias = packages[0].asname
+      
+      if alias and re.search(self.PACKAGE_STYLE, alias):
+        exception = Exception(
                     line=node.lineno,
-                    txt='{name!r}',
-                    msg='Imported module does not follow PEP8 Standards',
-                    expl='Review PEP8 style for modules'
-      )
-      self.exceptions.add(exception)
+                    txt=f'import {alias}',
+                    msg='Alias name does not follow PEP8 Standards',
+                    expl='Review PEP8 style for module importing'
+        )
+        
+        self.exceptions.add(exception)
+
     super().generic_visit(node)
   
 class VariableScopeUsage(Rule):
   def __init__(self):
     self.unused = collections.Counter()
     self.names = collections.Counter()
+
     super().__init__()
 
   def visit_Name(self, node):
@@ -210,30 +276,33 @@ class Variable(Rule):
         node = visitor.names[name]
         exception = Exception(
                       line=node.lineno,
-                      text='The variable {node!r} has not been used',
+                      text=f'The variable {node} has not been used',
                       msg='Unused variable',
                       expl='Your previously defined variable has not been used'
         )
+
         self.exceptions.add(exception)
   
   def visit_FunctionDef(self, node):
     self.lint_variables(node)
+
     super().generic_visit(node)
 
   def visit_ClassDef(self, node):
     self.lint_variables(node)
+
     super().generic_visit(node)
   
   def visit_Module(self, node):
     self.lint_variables(node)
+
     super().generic_visit(node)
 
 if __name__ == '__main__':
   source_path = sys.argv[1]
 
   linter = Linter()
-  linter.rules.add(Sets())
-  linter.rules.add(Naming())
+  linter.rules.add(Imports())
   
   print('Linting...')
   linter.run(source_path)
